b6011a5c559e01d830eb97ccc1b9055a425ed9f2
Dylan <dylanpdx@gmail.com>
2022-03-22 14:45:37 +0000

    Save logged in user to local storage, functions for checking user role

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { User } from '../dto/User';
 import { tap } from "rxjs/operators";
 import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,7 @@ export class UsersService {
 
   url: string = "https://nyyvg3k62g.execute-api.us-east-1.amazonaws.com/stage-2";
 
-  constructor(private http: HttpClient, public auth: AuthService)
+  constructor(private http: HttpClient, public auth: AuthService,private router: Router)
   { 
 
   }
@@ -28,5 +29,30 @@ export class UsersService {
   getUser()
   {
     return this.http.get<User>(this.url+"/getUser?username="+localStorage.getItem("username"));
+  }
+
+  getLoggedInUserRole(){
+    var loggedInUserData=localStorage.getItem("loggedInUser");
+    if (loggedInUserData == null)
+    {
+      return "";
+    }
+    var cUser : User = JSON.parse(loggedInUserData);
+    return cUser.userType;
+  }
+
+  checkLoggedInUserIs(checkRole : string) : boolean{
+    // checks if logged in user is of the specified role, returns true/false
+    var role = this.getLoggedInUserRole();
+    return role == checkRole;
+  }
+
+  roleGuard(checkRole : string){
+    // if user is not of the specified role, redirect to games page
+    if (!this.checkLoggedInUserIs(checkRole))
+    {
+      alert("You are not authorized to view this page.");
+      this.router.navigate(['games']);
+    }
   }
 }
