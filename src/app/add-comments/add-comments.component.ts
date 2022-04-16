add74f03a9ae38b2fc521e9323d6b91ec3ec5b6b
Dougzz19 <kevindoug2019@gmail.com>
2022-04-16 16:25:35 +0100

    add-comments component

--- src/app/add-comments/add-comments.component.ts
+++ src/app/add-comments/add-comments.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { Game } from '../dto/Game';
+import { GamesService } from '../services/games.service';
 
 @Component({
   selector: 'app-add-comments',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCommentsComponent implements OnInit {
 
-  constructor() { }
+  @Input() game : Game;
+  comment : string = "";
+
+
+  constructor(private gameService : GamesService) {
+    this.game = Game.generateEmptyGame();
+  }
 
   ngOnInit(): void {
+  }
+
+  addComments(){
+    if(this.game.gameID=="")
+      alert("Please select a game!");
+    else if(this.comment=="")
+      alert("Please Enter Comment");
+    else{
+      var toAdd = this.comment;
+      this.gameService.addComments(this.game.gameID, toAdd).subscribe();
+      this.comment = ""
+    }
   }
 
 }
